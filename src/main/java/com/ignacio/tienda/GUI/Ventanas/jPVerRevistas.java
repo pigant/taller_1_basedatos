5a8d35291b7c24bd842bf8a5c2223d852a53252b
ignacio <ignacio.ruz.donoso@gmail.com>
2015-11-26 14:01:51 -0300

    Se repara el formato de ver venta Se ve la cantidad y el dinero

--- src/main/java/com/ignacio/tienda/GUI/Ventanas/jPVerRevistas.java
+++ src/main/java/com/ignacio/tienda/GUI/Ventanas/jPVerRevistas.java
@@ -32,10 +32,12 @@ public class jPVerRevistas extends javax.swing.JPanel {
         java.awt.GridBagConstraints gridBagConstraints;
 
         jB_verRevistas = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        jL_cantidad = new javax.swing.JLabel();
         jL_dinero = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jT_comics = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setPreferredSize(new java.awt.Dimension(400, 400));
         setLayout(new java.awt.GridBagLayout());
@@ -49,28 +51,28 @@ public class jPVerRevistas extends javax.swing.JPanel {
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(0, 15, 0, 0);
-        add(jB_verRevistas, gridBagConstraints);
-
-        jButton2.setText("Contar");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 2;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHEAST;
-        gridBagConstraints.weightx = 1.9;
-        add(jButton2, gridBagConstraints);
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 3;
-        gridBagConstraints.gridy = 0;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
         gridBagConstraints.weightx = 0.3;
+        gridBagConstraints.insets = new java.awt.Insets(0, 15, 0, 0);
+        add(jB_verRevistas, gridBagConstraints);
+
+        jL_cantidad.setText("0");
+        jL_cantidad.setToolTipText("");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 0.1;
+        add(jL_cantidad, gridBagConstraints);
+
+        jL_dinero.setText("0");
+        jL_dinero.setToolTipText("");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 4;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 0.1;
         gridBagConstraints.insets = new java.awt.Insets(0, 0, 0, 15);
         add(jL_dinero, gridBagConstraints);
 
@@ -90,15 +92,31 @@ public class jPVerRevistas extends javax.swing.JPanel {
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
-        gridBagConstraints.gridwidth = 4;
+        gridBagConstraints.gridwidth = 5;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.ipadx = 579;
-        gridBagConstraints.ipady = 403;
+        gridBagConstraints.ipadx = 357;
+        gridBagConstraints.ipady = 319;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
-        gridBagConstraints.insets = new java.awt.Insets(15, 15, 15, 15);
+        gridBagConstraints.insets = new java.awt.Insets(5, 15, 0, 15);
         add(jScrollPane1, gridBagConstraints);
+
+        jLabel1.setText("Cantidad:");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+        gridBagConstraints.weightx = 0.25;
+        add(jLabel1, gridBagConstraints);
+
+        jLabel2.setText("Dinero: $");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+        gridBagConstraints.weightx = 0.25;
+        add(jLabel2, gridBagConstraints);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jB_verRevistasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jB_verRevistasActionPerformed
@@ -117,8 +135,11 @@ public class jPVerRevistas extends javax.swing.JPanel {
 					};
 					dtm.addRow(o);
 				}
-				jT_comics.setModel(dtm);
 				JOptionPane.showMessageDialog(this, "Se cargaron los comics");
+				jT_comics.setModel(dtm);
+				int cantidad = dtm.getRowCount();
+				jL_dinero.setText(String.valueOf(cantidad * 1000));
+				jL_cantidad.setText(String.valueOf(cantidad));
 			} else {
 				JOptionPane.showMessageDialog(this,
 						"No se cargaron los datos de los comics",
@@ -130,17 +151,13 @@ public class jPVerRevistas extends javax.swing.JPanel {
 		}
     }//GEN-LAST:event_jB_verRevistasActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-		TableModel dtm = jT_comics.getModel();
-		int dinero = dtm.getRowCount() * 1000;
-		jL_dinero.setText(String.valueOf(dinero));
-    }//GEN-LAST:event_jButton2ActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jB_verRevistas;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jL_cantidad;
     private javax.swing.JLabel jL_dinero;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jT_comics;
     // End of variables declaration//GEN-END:variables
